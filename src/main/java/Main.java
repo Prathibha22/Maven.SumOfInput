a77ea5fa52fc47e1f449df10f234be3f579d5acf
Prathibha <prathibha@Zipcoders-MacBook-Pro-8.local>
2022-10-31 12:06:23 -0400

    finished sum of methods using 2 monthods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,31 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-
+       System.out.println("Please enter a number");
+        Scanner input=new Scanner(System.in);
+        int number=input.nextInt();
+        int result1=sumOfNumbers(number);
+        int result2=gaussianSumOfNumbers(number);
+        System.out.println("The sum of n numbers is "+ result1);
+        System.out.println("The sum of n numbers using gaussian method is "+result2);
     }
     
-    int sumOfNumbers(int n) {
-        return 0;
+    static int sumOfNumbers(int n) {
+        int sum=0;
+        for(int i=1;i<=n;i++)
+            sum+=i;
+        return sum;
     }
     
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
-        return 0;
+     static int gaussianSumOfNumbers(int n) {
+
+        return (n*(n+1))/2;
     }
    
     // for Extra extra credit - compare the methods and show which one is faster
